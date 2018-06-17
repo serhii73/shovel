28fa6772ec0b34d047a76c4cc668a088108864db
sergey <aserhii@protonmail.com>
2018-06-17 21:38:33 +0300

    Fix doc in shovel.py

--- shovel.py
+++ shovel.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
-"""Shovel project.
+"""Shovel module.
 
-This project shows what a shovel is, why it is needed,
+This module shows what a shovel is, why it is needed,
 what can be done with it, its characteristics are shown.
 """
 from math import pi
 
 
 class Shovel:
-    """A shovel is a tool for digging, lifting, and moving bulk materials.
+    """A shovel is a tool for remove debris and for planting potatoes.
 
     Also, a shovel is an element of self-defense.
+    Also, we can calculate the area of handle shovel.
     """
 
     def __init__(self, color, weight, length, diameter):
@@ -21,40 +22,30 @@ class Shovel:
         self.diameter = diameter
 
     def area_handle(self):
-        """Area handle shovel.
-
-        Returns:
-            prints the cylinder area.
-
-        """
+        """Print the cylinder(handle shovel) area."""
         print('Area shovel handle is {}.'.format(
             2 * pi * (self.diameter / 2) * self.length))
 
     def protect_me(self):
         """Protection from the robber.
 
-        Returns:
-            prints the shovel weight.
-
+        If the shovel is heavy, the robber can be killed.
         """
         print('Hit the robber. {} kilo - will hurt.'.format(self.weight))
+        if self.weight > 5:
+            print('Call 911, the robber unconscious.')
 
     @staticmethod
     def plant_potatoes():
-        """Put the potatoes.
-
-        Returns:
-            prints the action - plant potatoes.
-
-        """
-        print('Plant potatoes.')
+        """Put the potatoes in the fertile land."""
+        print('Plant potatoes on our field.')
 
     @staticmethod
-    def remove_debris():
+    def remove_debris(weight_debris):
         """Remove debris.
 
-        Returns:
-            prints the action - remove debris.
-
+        If debris is heavy, the shovel can break.
         """
-        print('Remove debris.')
+        print('Pick up trash near our yard.')
+        if weight_debris > 4:
+            print('Be careful, the debris is heavy.')
