2a875f4d449a93834bf1df272f35993937bebe21
sergey <aserhii@protonmail.com>
2018-06-17 10:38:12 +0300

    Update doc Fix class doc and add more info to another places.

--- shovel.py
+++ shovel.py
@@ -1,9 +1,17 @@
-"""Shovel project."""
+# -*- coding: utf-8 -*-
+"""Shovel project.
+
+This project shows what a shovel is, why it is needed,
+what can be done with it, its characteristics are shown.
+"""
 from math import pi
 
 
 class Shovel:
-    """Shovel parameters and actions that can be implemented with it."""
+    """A shovel is a tool for digging, lifting, and moving bulk materials.
+
+    Also, a shovel is an element of self-defense.
+    """
 
     def __init__(self, color, weight, length, diameter):
         """Shovel parameters."""
@@ -13,20 +21,40 @@ class Shovel:
         self.diameter = diameter
 
     def area_handle(self):
-        """Area handle shovel."""
+        """Area handle shovel.
+
+        Returns:
+            prints the cylinder area.
+
+        """
         print('Area shovel handle is {}.'.format(
             2 * pi * (self.diameter / 2) * self.length))
 
     def protect_me(self):
-        """Protection from the robber."""
+        """Protection from the robber.
+
+        Returns:
+            prints the shovel weight.
+
+        """
         print('Hit the robber. {} kilo - will hurt.'.format(self.weight))
 
     @staticmethod
     def plant_potatoes():
-        """Put the potatoes."""
+        """Put the potatoes.
+
+        Returns:
+            prints the action - plant potatoes.
+
+        """
         print('Plant potatoes.')
 
     @staticmethod
     def remove_debris():
-        """Remove debris."""
+        """Remove debris.
+
+        Returns:
+            prints the action - remove debris.
+
+        """
         print('Remove debris.')
